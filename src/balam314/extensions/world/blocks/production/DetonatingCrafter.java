9b0e797c6a2ba5ba2bacdeb34905b06b7c2ffbb3
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2022-10-10 19:03:32 +0530

    Complete DetonatingCrafter

--- src/balam314/extensions/world/blocks/production/DetonatingCrafter.java
+++ src/balam314/extensions/world/blocks/production/DetonatingCrafter.java
@@ -1,23 +1,34 @@
 package balam314.extensions.world.blocks.production;
 
-import balam314.extensions.register.EItems;
+import arc.audio.Sound;
+import arc.graphics.Color;
+import arc.math.Mathf;
+import mindustry.Vars;
 import mindustry.content.Fx;
-import mindustry.content.Items;
 import mindustry.entities.Damage;
 import mindustry.entities.Effect;
 import mindustry.gen.Sounds;
-import mindustry.type.Item;
+import mindustry.graphics.Pal;
+import mindustry.ui.Bar;
 import mindustry.world.blocks.production.GenericCrafter;
-import mindustry.Vars;
-import mindustry.world.meta.Stat;
+import mindustry.world.consumers.Consume;
+
+import static mindustry.Vars.tilesize;
 
 
-/**A crafter that detonates if it has insufficient supply of an item. TODO finish*/
+/**A crafter that detonates if it has insufficient supply of an item. */
 public class DetonatingCrafter extends GenericCrafter {
-	public Item antiDetonationItem = Items.phaseFabric;
+	public Consume antiDetonationItem;
 	public int explosionRadius = 3;
 	public int explosionDamage = 100;
-	public Effect explodeEffect = Fx.reactorExplosion;//TODO this is probably a bit much
+	public Effect explodeEffect = Fx.massiveExplosion;
+	/** Time to detonate*/
+	public float explosionTime = 2f;
+	public float explosionShake = 0f;
+	public float explosionShakeDuration = 12f;
+	public Sound explodeSound = Sounds.explosion;
+	public String explosionCauserName = "Sus level";
+	public Color explosionBarColor = Color.lime;
 
 
 
@@ -29,32 +40,52 @@ public class DetonatingCrafter extends GenericCrafter {
 	@Override
 	public void setStats() {
 		super.setStats();
-		stats.add(Stat.boostEffect, antiDetonationItem);
+		//TODO add anti detonation item
+	}
+
+	@Override
+	public void setBars() {
+		super.setBars();
+		addBar(explosionCauserName, (DetonatingCrafterBuild b) -> new Bar("bar." + explosionCauserName, explosionBarColor, () -> b.explosionTimer / (explosionTime * 60f)));
 	}
 
 	public class DetonatingCrafterBuild extends GenericCrafterBuild {
 
-		public float badThingThatCausesDetonation = 0;
+		public float explosionTimer = 0f;
 
 		@Override
 		public void updateTile() {
+			float antidetonationEfficiency = antiDetonationItem.efficiency(this);
+			if(antidetonationEfficiency == 1f){
+				explosionTimer --;
+				if(explosionTimer < 0) explosionTimer = 0;
+			} else if(this.efficiency > 0){
+				explosionTimer += (1 - antidetonationEfficiency) * this.efficiency;
+			}
+			if(explosionTimer >= explosionTime * 60f) this.kill();
 			super.updateTile();
 		}
 
-		@Deprecated
-		public void detonate(){
-			this.kablooey();
+		@Override
+		public void onDestroyed(){
+			super.onDestroyed();
+
+			if(Vars.state.rules.reactorExplosions){
+				createExplosion();
+			}
 		}
 
-		public void kablooey(){
+		public void createExplosion(){
+			if(explosionDamage > 0){
+				Damage.damage(x, y, explosionRadius * tilesize, explosionDamage);
+			}
 
-			Sounds.explosion.at(this);
-			Damage.damage(x, y, explosionRadius * Vars.tilesize, explosionDamage * 4);
-			Effect.shake(2f, 16f, x, y);
+			explodeEffect.at(this);
+			explodeSound.at(this);
 
-			explodeEffect.at(x, y);
-
-			this.kill();
+			if(explosionShake > 0){
+				Effect.shake(explosionShake, explosionShakeDuration, this);
+			}
 		}
 	}
 }
