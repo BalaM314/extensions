bc105bc1bce7472f5b936177adb23300aa5351dc
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2023-01-15 12:19:02 +0530

    Fix load order

--- src/balam314/extensions/Extensions.java
+++ src/balam314/extensions/Extensions.java
@@ -35,7 +35,7 @@ public class Extensions extends Mod{
     public void loadContent(){
         util.log("Loading content.");
 
-        ContentList[] allContent = {new EItems(), new ELiquids(), new EBlocks(), new EStatusEffects()};
+        ContentList[] allContent = {new EItems(), new EStatusEffects(), new ELiquids(), new EBlocks()};
         for(ContentList list : allContent) {
             list.load();
             util.log("Loaded " + list.getClass().getName());
