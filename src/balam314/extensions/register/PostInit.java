3156b5a24545861369cc1a94fb7270e6dd2267c1
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2023-01-15 12:26:57 +0530

    Allow Tsunami to shoot Advanced Coolant

--- src/balam314/extensions/register/PostInit.java
+++ src/balam314/extensions/register/PostInit.java
@@ -14,6 +14,16 @@ import static balam314.extensions.register.ELiquids.*;
 public class PostInit {
 
 	public static void load(){
-		
+		((LiquidTurret)tsunami).ammoTypes.put(advancedCoolant, new LiquidBulletType(ELiquids.advancedCoolant){{
+			lifetime = 49f;
+			speed = 4f;
+			knockback = 1.3f;
+			puddleSize = 8f;
+			orbSize = 4f;
+			drag = 0.001f;
+			ammoMultiplier = 2.2f;
+			statusDuration = 60f * 5f;
+			damage = 0.2f;
+		}});
 	}
 }
